7fb0880e5c558329893f31a4997d48b3b35f2598
Hisham Alkahtani <AlkahtaniHisham@gmail.com>
2025-01-15 16:54:55 +0300

    Make server assign clients to threads from thread pool

--- src/LoadBalancer.cpp
+++ src/LoadBalancer.cpp
@@ -9,31 +9,90 @@
 
 class LoadBalancer
 {
-    std::vector<HttpServerThread *> clientThreads;
+    HttpServerThread *threadPool;
+
+    InternetHttpSocket *clientQueue;
+    int queueHead;
+    int queueTail;
+    int queueLength;
+
     std::thread loadBalancerThread;
 
     int maxThreads;
+    int activeThreads;
 
 public:
     LoadBalancer(int maxThreads) : maxThreads(maxThreads)
     {
+        queueLength = 1024;
+        activeThreads = 0;
+        queueHead = 0;
+        queueTail = 0;
+        activeThreads = 0;
+
+        // queue size is 1024 for now, could be configurable later
+        clientQueue = new InternetHttpSocket[queueLength]();
+        threadPool = new HttpServerThread[maxThreads]();
         loadBalancerThread = std::thread(std::ref(*this));
     }
 
-    // The actual assigning of threads will be performed on it's own threads.
+    // The actual assigning of threads will be performed on it's own thread.
+    // Takes clients from head of queue, and assigns them to available threads
     void operator()()
     {
-        // std::cout << "Load Balancing Thread Started!" << std::endl;
+        std::cout << "Load Balancing Thread Started!" << std::endl;
+
+        while (true)
+        {
+            if (queueHead == queueTail)
+            {
+                // This might be a bottleneck, measure performance and see if it needs
+                // something more sophisticated, maybe a condition variable?
+                std::cout << "LBThread sleeping because n oclients" << queueHead << queueTail << std::endl;
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+                continue;
+            }
+
+            bool clientAssigned = false;
+            for (int i = 0; i < activeThreads; i++)
+            {
+                if (!threadPool[i].isActive)
+                {
+                    threadPool[i].assignClient(clientQueue[queueHead]);
+                    clientAssigned = true;
+                }
+            }
+
+            if (!clientAssigned && activeThreads < maxThreads)
+            {
+                threadPool[activeThreads].assignClient(clientQueue[queueHead]);
+                threadPool[activeThreads++].startThread();
+                clientAssigned = true;
+            }
+
+            if (clientAssigned)
+                queueHead = (queueHead + 1) % queueLength;
+        }
     }
 
-    void enqueu_socket(InternetHttpSocket socket)
+    void
+    enqueu_socket(InternetHttpSocket socket)
     {
         std::cout << "Client enqueu call" << std::endl;
-
-        // temp implementation until queue is implemeneted new thread per request.
-        // shit's leaky
-
-        HttpServerThread *thread = new HttpServerThread(socket);
-        clientThreads.push_back(thread);
+        bool clientAdded = false;
+        while (!clientAdded)
+        {
+            if ((queueTail + 1) % queueLength == queueHead)
+            {
+                // to prevent adding when queue is full...
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            }
+            else
+            {
+                clientQueue[queueTail] = socket;
+                queueTail = (queueTail + 1) % queueLength;
+                clientAdded = true;
+            }
+        }
     }
 };
