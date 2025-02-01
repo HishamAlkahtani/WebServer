34dd419f00f1de7d3bfab86bef81b56559b61d2a
Hisham Alkahtani <AlkahtaniHisham@gmail.com>
2025-02-01 13:54:04 +0300

    Refactor HTTP request handling code

---
+++ src/HttpRequestHandler.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <filesystem>
+#include <fstream>
+#include <memory>
+#include <string>
+#include <iostream>
+#include <unistd.h>
+#include <cstdlib>
+#include <cstring>
+#include "http.hpp"
+
+class HttpRequestHandler
+{
+    std::string workingDirectory;
+
+    // Check if file is under server root.
+    // Returns false if file exists and is out of bounds, true otherwise.
+    bool safe_path(std::string path);
+
+    void initWorkingDirectory();
+
+public:
+    HttpRequestHandler();
+
+    std::unique_ptr<HttpResponse> createResponsee(HttpRequest request);
+};
+
+HttpRequestHandler &getRequestHandler();
